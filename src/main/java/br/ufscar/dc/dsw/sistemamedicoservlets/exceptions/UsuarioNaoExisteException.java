4a6420e8005c67d5ca208a1987a8d91c82eb1227
Gustavo <geraldellig@gmail.com>
2024-07-09 20:50:49 -0300

    add UsuarioNaoExisteException

---
+++ src/main/java/br/ufscar/dc/dsw/sistemamedicoservlets/exceptions/UsuarioNaoExisteException.java
@@ -0,0 +1,9 @@
+package br.ufscar.dc.dsw.sistemamedicoservlets.exceptions;
+
+public class UsuarioNaoExisteException extends Exception {
+
+    public UsuarioNaoExisteException(String msg) {
+        super(msg);
+    }
+
+}
